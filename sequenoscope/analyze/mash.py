21b584ba1d4370a6ebb82f99a95bdfc754b937f0
ameknas <abdallahmeknas@gmail.com>
2023-10-11 15:08:44 -0400

    added comments

--- sequenoscope/analyze/mash.py
+++ sequenoscope/analyze/mash.py
@@ -4,11 +4,30 @@ import os
 
 class MashSketcher:
     def __init__(self, out_directory, out_prefix):
+        """
+        Initialize the class with out_directory, out_prefix.
+
+        Arguments:
+            out_directory: str
+                A string to the path where the output files will be stored.
+            out_prefix: str
+                A designation of what the output files will be named.
+        """
         self.results = {}
         self.out_directory = out_directory
         self.out_prefix = out_prefix
 
     def run_mash_sketch(self, input_files):
+        """
+        Run the MASH sketch process on the input files. Can handle either single or paired input.
+
+        Arguments:
+            input_files: list
+                List containing paths to the input file(s).
+
+        Returns:
+            dict: Dictionary containing results of the MASH sketch process.
+        """
         if len(input_files) == 1:
             self.results = self._sketch_single(input_files[0], self.out_prefix)
         elif len(input_files) == 2:
@@ -18,6 +37,18 @@ class MashSketcher:
         return self.results
 
     def _sketch_single(self, input_file, file_prefix):
+        """
+        Run MASH sketch on a single input file and extracts the genome size and coverage.
+
+        Arguments:
+            input_file: str
+                Path to the input file.
+            file_prefix: str
+                Prefix for the output file.
+
+        Returns:
+            dict: Dictionary containing genome size and coverage.
+        """
         out_file_hash = os.path.join(self.out_directory, f"{file_prefix}_mash_hash")
         cmd = ["mash", "sketch", "-r", input_file, "-o", out_file_hash, "-k", "27"]
         result = subprocess.run(cmd, capture_output=True, text=True, check=True)
@@ -37,6 +68,18 @@ class MashSketcher:
         return mash_results
 
     def _sketch_paired(self, input_file1, input_file2):
+        """
+        Run MASH sketch on paired input files and computes the average genome size and coverage.
+        
+        Arguments:
+            input_file1: str
+                Path to the first input file.
+            input_file2: str
+                Path to the second input file.
+
+        Returns:
+            dict: Dictionary with the average genome size and coverage.
+        """
         results1 = self._sketch_single(input_file1, f"{self.out_prefix}_1")
         results2 = self._sketch_single(input_file2, f"{self.out_prefix}_2")
         
